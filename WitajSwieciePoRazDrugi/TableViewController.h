d1cbc9c08e81efdfa662ee60e768fc0a5f95bdda
iBednar <p.bednarczyk@icloud.com>
2014-08-02 16:55:58 +0200

    Witaj Świecie

---
+++ WitajSwieciePoRazDrugi/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  WitajSwieciePoRazDrugi
+//
+//  Created by Pawel Bednarczyk on 02.08.2014.
+//  Copyright (c) 2014 iOSTutki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
---
+++ WitajSwieciePoRazDrugiTests/WitajSwieciePoRazDrugiTests.m
@@ -0,0 +1,34 @@
+//
+//  WitajSwieciePoRazDrugiTests.m
+//  WitajSwieciePoRazDrugiTests
+//
+//  Created by Pawel Bednarczyk on 02.08.2014.
+//  Copyright (c) 2014 iOSTutki. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WitajSwieciePoRazDrugiTests : XCTestCase
+
+@end
+
+@implementation WitajSwieciePoRazDrugiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
